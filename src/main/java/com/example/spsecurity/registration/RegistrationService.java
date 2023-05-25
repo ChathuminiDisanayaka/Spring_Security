fa7c52cc04b18922b81e4ca8c9c1185a64f0d83d
Rumesha Disanayaka <2019cs017@stu.ucsc.lk>
2023-05-25 15:03:29 +0530

    Updated RegistrationService

--- src/main/java/com/example/spsecurity/registration/RegistrationService.java
+++ src/main/java/com/example/spsecurity/registration/RegistrationService.java
@@ -1,11 +1,33 @@
 package com.example.spsecurity.registration;
 
+import com.example.spsecurity.appuser.AppUserRole;
+import com.example.spsecurity.appuser.AppUserService;
+import com.example.spsecurity.appuser.Appuser;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
+    private final AppUserService appUserService;
+    private EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail =
+                emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("Email bot valid");
+        }
+        return appUserService.signUpUser(
+                new Appuser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                        )
+        );
     }
 }
